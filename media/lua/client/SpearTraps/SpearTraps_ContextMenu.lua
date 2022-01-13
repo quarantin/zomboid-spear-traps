946ce33da62de35b7b04518e4e802356cc727161
Corentin Delorme <codelorme@gmail.com>
2022-01-13 14:13:00 +0100

    Update SpearTraps_ContextMenu.lua: Aggregate spears in context menu + Allow adding/removing more than one spear at once

--- media/lua/client/SpearTraps/SpearTraps_ContextMenu.lua
+++ media/lua/client/SpearTraps/SpearTraps_ContextMenu.lua
@@ -17,23 +17,68 @@ local function getClosestSquare(player, grave)
 	return d1 < d2 and sq1 or sq2
 end
 
-local function onAddSpear(worldobjects, grave, spear, player)
+local function onAddSpear(worldobjects, grave, items, player)
 	local closestSquare = getClosestSquare(player, grave)
 	if luautils.walkAdj(player, closestSquare, false) then
 		local primary = true
 		local twoHands = false
-		ISWorldObjectContextMenu.equip(player, player:getPrimaryHandItem(), spear, primary, twoHands)
-		ISTimedActionQueue.add(ISAddSpearToGrave:new(player, grave, spear, 100))
+		local spears = {}
+		for i = 1, math.min(5, #items) do
+			table.insert(spears, items[i])
+		end
+		for spearIndex, spear in pairs(spears) do
+			ISWorldObjectContextMenu.equip(player, player:getPrimaryHandItem(), spear, primary, twoHands)
+			ISTimedActionQueue.add(ISAddSpearToGrave:new(player, grave, spear, 100))
+		end
 	end
 end
 
-local function onRemoveSpear(worldobjects, grave, spear, spearIndex, player)
+local function onRemoveSpear(worldobjects, grave, spears, player)
 	local closestSquare = getClosestSquare(player, grave)
 	if luautils.walkAdj(player, closestSquare, false) then
 		local primary = true
 		local twoHands = false
-		ISTimedActionQueue.add(ISRemoveSpearFromGrave:new(player, grave, spear, spearIndex, 100))
+		for spearIndex, spear in ipairs(spears) do
+			ISTimedActionQueue.add(ISRemoveSpearFromGrave:new(player, grave, spear, spearIndex, 100))
+		end
 	end
+end
+
+local function aggregateItems(items)
+
+	local aggregate = {}
+
+	for i = 0, items:size() - 1 do
+
+		local item = items:get(i)
+		local itemName = item:getName()
+		if not aggregate[itemName] then
+			aggregate[itemName] = {}
+		end
+		table.insert(aggregate[itemName], item)
+	end
+
+	return aggregate
+end
+
+local function aggregateItems2(items)
+
+	local aggregate = {}
+
+	for _, item in ipairs(items) do
+
+		local itemName = item.name
+		if item.condition <= 0 then
+			itemName = itemName .. ' (Broken)'
+		end
+
+		if not aggregate[itemName] then
+			aggregate[itemName] = {}
+		end
+		table.insert(aggregate[itemName], item)
+	end
+
+	return aggregate
 end
 
 local function onFillWorldObjectContextMenu(player, context, worldobjects, test)
@@ -54,6 +99,8 @@ local function onFillWorldObjectContextMenu(player, context, worldobjects, test)
 		return
 	end
 
+	items = aggregateItems(items)
+
 	for i,v in ipairs(worldobjects) do
 		local square = v:getSquare()
 		for i=0, square:getSpecialObjects():size()-1 do
@@ -63,13 +110,17 @@ local function onFillWorldObjectContextMenu(player, context, worldobjects, test)
 				local spears = data['spears'] or {}
 				local corpses = data['corpses'] or 0
 				local maxCorpses = ISEmptyGraves.getMaxCorpses(grave)
+
 				if #spears < maxCorpses and corpses < maxCorpses then
 					local rootmenu = context:addOption(getText('ContextMenu_AddSpearToGrave'), worldobjects, nil)
 					local submenu = context:getNew(context)
 					context:addSubMenu(rootmenu, submenu)
-					for i = 0, items:size() - 1 do
-						local spear = items:get(i)
-						submenu:addOption(spear:getName(), worldobjects, onAddSpear, grave, spear, playerObj)
+					for name, spears in pairs(items) do
+						local itemName = name
+						if #spears > 1 then
+							itemName = itemName .. ' (' .. tostring(#spears) .. ')'
+						end
+						submenu:addOption(itemName, worldobjects, onAddSpear, grave, spears, playerObj)
 					end
 					return
 				end
@@ -91,17 +142,18 @@ local function onFillWorldObjectContextMenu2(player, context, worldobjects, test
 			local grave = square:getSpecialObjects():get(i)
 			if grave:getName() == 'EmptyGraves' then
 				local data = grave:getModData() or {}
-				local spears = data['spears'] or {}
-				if #spears > 0 then
+				local items = data['spears'] or {}
+				if #items > 0 then
 					local rootmenu = context:addOption(getText('ContextMenu_RemoveSpearFromGrave'), worldobjects, nil)
 					local submenu = context:getNew(context)
 					context:addSubMenu(rootmenu, submenu)
-					for spearIndex, spear in pairs(spears) do
-						local name = spear.name
-						if spear.condition <= 0 then
-							name = name .. ' (Broken)'
+					items = aggregateItems2(items)
+					for name, spears in pairs(items) do
+						local itemName = name
+						if #spears > 1 then
+							itemName = itemName .. ' (' .. tostring(#spears) .. ')'
 						end
-						submenu:addOption(name, worldobjects, onRemoveSpear, grave, spear, spearIndex, playerObj)
+						submenu:addOption(itemName, worldobjects, onRemoveSpear, grave, spears, playerObj)
 					end
 					return
 				end
