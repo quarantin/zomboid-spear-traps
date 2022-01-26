f16bbfe52f9ce1fa54018287153e5d4c484541ad
Corentin Delorme <codelorme@gmail.com>
2022-01-26 22:33:07 +0100

    Update SpearTraps.lua: Don't kill players when invincible or when god-mode is true

--- media/lua/client/SpearTraps/SpearTraps.lua
+++ media/lua/client/SpearTraps/SpearTraps.lua
@@ -180,7 +180,7 @@ end
 
 local function onPlayerUpdate(player)
 
-	if not player:isAlive() or player:getVehicle() then
+	if not player:isAlive() or player:getVehicle() or player:isGodMod() or player:isInvincible() then
 		return
 	end
 
